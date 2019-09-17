2e3a5f47fc090e2e46ef69cc8cb21bbd80643bfe
emmegha <41463313+emmegha@users.noreply.github.com>
2019-09-17 22:39:53 +0530

    Pattern

---
+++ Pattern1.cpp
@@ -0,0 +1,33 @@
+*/Write a program to print this pattern:
+          1
+        2 3
+      4 5 6
+    7 8 9 10
+    
+/*
+
+#include<iostream..h>
+#include<conio.h>
+void main()
+{   
+      int i,b=1,j;
+      clrscr();
+      cout<<"\nPattern Is:\n";
+      for(i=0;i<4;i++)
+      {   
+          for(j=0;j<4;j++)
+          {
+            if((i+j)>=3)
+            { 
+                    cout<<b;
+                    b++;
+             }
+             else
+             cout<<" ";
+       }
+       cout<<endl;
+       
+      }
+      getch();
+      
+  }
